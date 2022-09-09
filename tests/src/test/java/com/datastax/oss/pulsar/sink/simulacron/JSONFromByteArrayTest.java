1d631eeea5de0fbfc27aa803ce0f93e40f83a426
Ayman Khalil <ayman.khalil@datastax.com>
2022-09-08 23:23:19 -0700

    Remove unwanted changes

--- tests/src/test/java/com/datastax/oss/pulsar/sink/simulacron/JSONFromByteArrayTest.java
+++ tests/src/test/java/com/datastax/oss/pulsar/sink/simulacron/JSONFromByteArrayTest.java
@@ -25,11 +25,12 @@ import com.datastax.oss.driver.internal.core.data.DefaultUdtValue;
 import com.datastax.oss.dsbulk.tests.ccm.CCMCluster;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+
+import com.google.common.collect.ImmutableSet;
 import org.apache.pulsar.client.api.Producer;
 import org.apache.pulsar.client.api.PulsarClientException;
 import org.awaitility.Awaitility;
@@ -39,16 +40,15 @@ public class JSONFromByteArrayTest extends PulsarCCMTestBase {
 
   private static final String MAPPING =
       "a=key, b=value.field1, d=value.mapField, e=value.listField, f=value.udtField, h=value.setField, "
-          + "i=value.listOfMaps, j=value.setOfMaps, k=value.mapOfLists, l=value.mapOfSets, m=value.setOfLists, n=value.listOfSets";
+              + "i=value.listOfMaps, j=value.setOfMaps, k=value.mapOfLists, l=value.mapOfSets, m=value.setOfLists, n=value.listOfSets";
 
-  private static final String JSON =
-      "{\"field1\":\"value1\",\"mapField\":{\"k1\":\"v1\",\"k2\":\"v2\"},\"listField\":[\"l1\",\"l2\"],\"setField\":[\"s1\",\"s2\"],"
-          + "\"listOfMaps\":[{\"k1\":\"v1\",\"k2\":\"v2\"}, {\"k1\":\"v1\",\"k2\":\"v2\"}],"
-          + "\"setOfMaps\":[{\"k1\":\"v1\",\"k2\":\"v2\"}],"
-          + "\"mapOfLists\":{\"k1\":[\"l1\",\"l2\"],\"k2\":[\"l1\",\"l2\"]},"
-          + "\"mapOfSets\":{\"k1\":[\"s1\",\"s2\"],\"k2\":[\"s1\",\"s2\"]},"
-          + "\"setOfLists\":[[\"l1\",\"l2\"]],"
-          + "\"listOfSets\":[[\"s1\",\"s2\"],[\"s1\",\"s2\"]],"
+  private static final String JSON = "{\"field1\":\"value1\",\"mapField\":{\"k1\":\"v1\",\"k2\":\"v2\"},\"listField\":[\"l1\",\"l2\"],\"setField\":[\"s1\",\"s2\"],"
+          +  "\"listOfMaps\":[{\"k1\":\"v1\",\"k2\":\"v2\"}, {\"k1\":\"v1\",\"k2\":\"v2\"}],"
+          +  "\"setOfMaps\":[{\"k1\":\"v1\",\"k2\":\"v2\"}],"
+          +  "\"mapOfLists\":{\"k1\":[\"l1\",\"l2\"],\"k2\":[\"l1\",\"l2\"]},"
+          +  "\"mapOfSets\":{\"k1\":[\"s1\",\"s2\"],\"k2\":[\"s1\",\"s2\"]},"
+          +  "\"setOfLists\":[[\"l1\",\"l2\"]],"
+          +  "\"listOfSets\":[[\"s1\",\"s2\"],[\"s1\",\"s2\"]],"
           + "\"udtField\":{\"intf\":99,\"stringf\":\"random\",\"mapf\":{\"k1\":7.0,\"k2\":9.0},\"listf\":[\"l1\",\"l2\"],\"setf\":[3,4]}}";
 
   public JSONFromByteArrayTest(CCMCluster ccm, CqlSession session) throws Exception {
@@ -70,7 +70,11 @@ public class JSONFromByteArrayTest extends PulsarCCMTestBase {
             .newProducer() // no schema
             .topic(pulsarSink.getTopic())
             .create()) {
-      producer.newMessage().key("838").value(JSON.getBytes(UTF_8)).send();
+      producer
+          .newMessage()
+          .key("838")
+          .value(JSON.getBytes(UTF_8))
+          .send();
     }
     try {
       Awaitility.waitAtMost(1, TimeUnit.MINUTES)
@@ -93,22 +97,18 @@ public class JSONFromByteArrayTest extends PulsarCCMTestBase {
         assertEquals(list, row.getList("e", String.class));
         assertEquals(set, row.getSet("h", String.class));
 
-        GenericType<List<Map<String, String>>> listOfMapsType =
-            new GenericType<List<Map<String, String>>>() {};
+        GenericType<List<Map<String, String>>> listOfMapsType = new GenericType<List<Map<String, String>>>() {};
         List<Map<String, String>> mapsList = row.get("i", listOfMapsType);
         assertEquals(ImmutableList.of(map, map), mapsList);
-        GenericType<Set<Map<String, String>>> setOfMapsType =
-            new GenericType<Set<Map<String, String>>>() {};
+        GenericType<Set<Map<String, String>>> setOfMapsType = new GenericType<Set<Map<String, String>>>() {};
         Set<Map<String, String>> mapsSet = row.get("j", setOfMapsType);
         assertEquals(ImmutableSet.of(map), mapsSet);
 
-        GenericType<Map<String, List<String>>> mapOfListsType =
-            new GenericType<Map<String, List<String>>>() {};
+        GenericType<Map<String, List<String>>> mapOfListsType = new GenericType<Map<String, List<String>>>() {};
         Map<String, List<String>> listsMap = row.get("k", mapOfListsType);
         assertEquals(ImmutableMap.of("k1", list, "k2", list), listsMap);
 
-        GenericType<Map<String, Set<String>>> mapOfSetsType =
-            new GenericType<Map<String, Set<String>>>() {};
+        GenericType<Map<String, Set<String>>> mapOfSetsType = new GenericType<Map<String, Set<String>>>() {};
         Map<String, Set<String>> setsMap = row.get("l", mapOfSetsType);
         assertEquals(ImmutableMap.of("k1", set, "k2", set), setsMap);
 
@@ -129,7 +129,7 @@ public class JSONFromByteArrayTest extends PulsarCCMTestBase {
         GenericType<Set<Integer>> udtSetType = new GenericType<Set<Integer>>() {};
         assertEquals(ImmutableSet.of(3, 4), value.get("setf", udtSetType));
         GenericType<Map<String, Double>> udtMapType = new GenericType<Map<String, Double>>() {};
-        assertEquals(ImmutableMap.of("k1", 7.0D, "k2", 9.0D), value.get("mapf", udtMapType));
+        assertEquals(ImmutableMap.of("k1", 7.0D,"k2",9.0D), value.get("mapf", udtMapType));
       }
       assertEquals(1, results.size());
     } finally {
